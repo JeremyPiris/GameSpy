183b20bd3209b82d187bff9f73fcf5b121ed4c58
Jeremy Piris <jeremypiris@hotmail.fr>
2022-03-17 20:50:55 +0100

    GameSpy Project

---
+++ streamlit_app/st_jp_test/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 14:29:40 2022
+
+@author: jeremy
+"""
+
+import streamlit as st
+
+st.image('/Users/jeremy/Documents/GitHub/Gamespy/streamlit_app/st_jp_test/gamespy_img.png')
+
